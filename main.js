f83f4953f35cb0421eaac71810d375e6a8bc0a2c
Anjali <97797520+Shakyaanjali@users.noreply.github.com>
2024-06-04 18:22:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+
+buttonsToggle.addEventListener('click', toggleButtons);
+
+function toggleButtons() {
+	buttonsToggle.classList.toggle('buttons__toggle--active');
+	buttonsComponent.classList.toggle('buttons--active');
+	document.activeElement.blur();
+}
